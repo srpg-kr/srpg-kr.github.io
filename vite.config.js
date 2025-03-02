a1acbd3e9ffc85a1715b139508b25c3b7340e299
jarari <hazardhax11@gmail.com>
2025-03-02 12:12:36 +0900

    attempt to fix multipage no3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        live2d: resolve(__dirname, 'live2d/index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@framework': '/framework'
