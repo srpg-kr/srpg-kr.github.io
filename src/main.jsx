98abd4ecd63622188a6763c838991eb539da8e42
jarari <hazardhax11@gmail.com>
2025-07-19 21:22:35 +0900

    additional script for 404 hack

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/translations" element={<Translations />} />
